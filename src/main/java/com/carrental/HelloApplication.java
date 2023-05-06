3dd5c24c2d10d5673acb0894e17773a6b071f633
SwiTzeX <nanisouhail@gmail.com>
2023-05-07 00:44:31 +0100

    test

--- src/main/java/com/carrental/HelloApplication.java
+++ src/main/java/com/carrental/HelloApplication.java
@@ -12,7 +12,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("home-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1400, 800);
-        stage.setTitle("Hello!");
+        stage.setTitle("Hello test!");
         stage.setScene(scene);
         stage.show();
     }
