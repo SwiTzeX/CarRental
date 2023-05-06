1341b750b2d084bca1866770447684fde8d93286
SwiTzeX <nanisouhail@gmail.com>
2023-05-06 14:57:28 +0100

    test

---
+++ src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module com.carrental.carrental {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens com.carrental.carrental to javafx.fxml;
+    exports com.carrental.carrental;
+}
