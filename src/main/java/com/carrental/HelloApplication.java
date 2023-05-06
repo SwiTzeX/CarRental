7b9e0224de4b55cacfa97caf960b78d506fc6644
SwiTzeX <nanisouhail@gmail.com>
2023-05-06 15:46:18 +0100

    test

--- src/main/java/com/carrental/HelloApplication.java
+++ src/main/java/com/carrental/HelloApplication.java
@@ -10,8 +10,8 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("test-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 640, 375);
         stage.setTitle("Hello World!");
         stage.setScene(scene);
         stage.show();
