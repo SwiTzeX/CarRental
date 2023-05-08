05f574d671e68edc2e203c3eb183413a56f62429
SwiTzeX <nanisouhail@gmail.com>
2023-05-09 00:50:57 +0100

    Home page update

--- src/main/java/com/carrental/HomeController.java
+++ src/main/java/com/carrental/HomeController.java
@@ -12,6 +12,8 @@ import javafx.scene.layout.VBox;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class HomeController implements Initializable {
@@ -31,6 +33,24 @@ public class HomeController implements Initializable {
     @FXML
     private Button previousPage;
     int maxPages;
+
+    public static <T> List<List<T>> split(Collection<T> data, int size)
+    {
+        List<List<T>> lists=new ArrayList<>((data.size()+size-1)/size);
+        List<T> list=new ArrayList<>(size);
+        for(T t:data)
+        {
+            list.add(t);
+            if(list.size()==size)
+            {
+                lists.add(list);
+                list=new ArrayList<>(size);
+            }
+        }
+        if(list.size()>0) lists.add(list);
+        return lists;
+    }
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         previousPage.setVisible(false);
@@ -42,6 +62,8 @@ public class HomeController implements Initializable {
         vehicles.add(new Vehicle("Volkswagen","Touareg","Family","Petrol","Manual",5,1000,140,120,220,"vehicles/volkswagen-touareg.png","brands/volkswagen.png"));
         vehicles.add(new Vehicle("Volkswagen","Touareg","Family","Petrol","Manual",5,1000,140,120,220,"vehicles/volkswagen-touareg.png","brands/volkswagen.png"));
         totalVeh.setText(String.valueOf(vehicles.size())+" Vehicle found");
+        List<List<Vehicle>> test = HomeController.split(vehicles,4);
+        System.out.println(test.get(1).get(1).getBrandName());
         maxPages = (int) vehicles.size()/4 +1;
         if (maxPages > 1){
             nextPage.setVisible(true);
