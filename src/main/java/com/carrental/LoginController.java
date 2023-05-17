dcf906ca086f8281979777e94b4bbf9f79e94119
7amid <hh@hh>
2023-05-17 20:11:28 +0100

    logincontroller update

--- src/main/java/com/carrental/LoginController.java
+++ src/main/java/com/carrental/LoginController.java
@@ -1,5 +1,18 @@
 package com.carrental;
 
-public class LoginController {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+import java.awt.*;
+
+public abstract class LoginController implements Initializable{
+
+    @FXML
+   private Button button_signin;
+
+    @FXML
+     Label label_login;
+
+
 }
 
