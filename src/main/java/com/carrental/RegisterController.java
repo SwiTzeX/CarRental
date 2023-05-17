0af67e303a39c3841c2d206ade6fcec4be4bcaf8
katokari <anas.chj12@gmail.com>
2023-05-18 00:16:36 +0200

    register-view.fxml

--- src/main/java/com/carrental/RegisterController.java
+++ src/main/java/com/carrental/RegisterController.java
@@ -1,5 +1,42 @@
 package com.carrental;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.Label;
+import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
+import javafx.event.ActionEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+
 public class RegisterController {
 
+    @FXML
+    private Label loginbtn;
+
+    @FXML
+    void goToLogin(MouseEvent event) {
+
+        try {
+            // Load the FXML file for the second view
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("login-view.fxml"));
+            Parent login = loader.load();
+
+            // Create a new stage and set the second view as the root
+            Stage stage =(Stage)((Node)event.getSource()).getScene().getWindow() ;
+            stage.setScene(new Scene(login));
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
