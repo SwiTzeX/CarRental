50e8d2c0da5345e56fa3ca1902b516f35c71707a
7amid <hh@hh>
2023-05-17 22:31:42 +0100

    logincontroller update

--- src/main/java/com/carrental/LoginController.java
+++ src/main/java/com/carrental/LoginController.java
@@ -2,8 +2,11 @@ package com.carrental;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.PasswordField;
 
+import javax.print.DocFlavor;
 import java.awt.*;
+import java.net.URL;
 
 public abstract class LoginController implements Initializable{
 
@@ -12,6 +15,14 @@ public abstract class LoginController implements Initializable{
 
     @FXML
      Label label_login;
+    @FXML
+    private TextField usernamefield;
+
+    @FXML
+    private PasswordField passwordfield;
+
+
+
 
 
 }
