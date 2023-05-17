b06f480bfc6ddafe479cc215283bcd92589e87b6
ghali <gmasmoud@gmail.com>
2023-05-17 17:56:24 +0100

    dashboard

--- src/main/java/com/carrental/HelloApplication.java
+++ src/main/java/com/carrental/HelloApplication.java
@@ -13,8 +13,8 @@ import java.sql.*;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("home-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 1400, 800);
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("dashboard-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 1250, 815);
         stage.setTitle("Hello test!");
         stage.setScene(scene);
         stage.setResizable(false);
