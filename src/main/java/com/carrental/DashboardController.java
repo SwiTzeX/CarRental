b3c06d7e18d72809abc7e491b645d03cba529d29
ghali <gmasmoud@gmail.com>
2023-05-19 13:52:31 +0100

    dashboard

--- src/main/java/com/carrental/DashboardController.java
+++ src/main/java/com/carrental/DashboardController.java
@@ -1,20 +1,34 @@
 package com.carrental;
 
+import com.carrental.models.User;
+import com.carrental.models.Vehicle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.XYChart;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class DashboardController implements Initializable {
-
+    @FXML
+    Label countCust;
+    @FXML
+    Label countTotalSales;
+    @FXML
+    Label countCars;
+    @FXML
+    Button reportButton;
     @FXML
     private LineChart<?, ?> lineChart;
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         iniLineChart();
+        getCountCust();
+        getCountCars();
+        //getCountTotalSales();
     }
 
 
@@ -28,5 +42,22 @@ public class DashboardController implements Initializable {
         lineChart.getData().addAll(series);
         lineChart.lookup(".chart-plot-background").setStyle("-fx-background-color:transparent");
         series.getNode().setStyle("-fx-stroke:#A390F0");
+    }
+
+    public void getCountCust() {
+        int countC = User.getAllUsers().size();
+        countCust.setText(String.valueOf(countC));
+    }
+
+    public void getCountCars() {
+        int countCar = Vehicle.getAllVehicles().size();
+        countCars.setText(String.valueOf(countCar));
+    }
+
+    /* public void getCountTotalSales() {
+
+    } */
+    public void reportButton(){
+
     }
 }
