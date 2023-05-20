a126242f54dc80a01e192e81098c7e83b377a8e2
7amid <hh@hh>
2023-05-21 00:05:43 +0100

    Users-view.fxml update

--- src/main/java/com/carrental/UsersController.java
+++ src/main/java/com/carrental/UsersController.java
@@ -1,6 +1,68 @@
 package com.carrental;
 
-public class UsersController{
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+
+import java.awt.event.ActionEvent;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.ResourceBundle;
+
+public class UsersController implements Initializable {
+
+    // admin - client
+    @FXML
+     private ComboBox<String> rules;
+
+    // ordre croissante et ordre decroissante
+
+    @FXML
+    private ComboBox<String> invoicedate;
+
+    // active - suspende - bloqueee
+    @FXML
+    private ComboBox<String> invoicestatue;
+
+    // add user
+    @FXML
+    private Button addUser;
+
+
+    // clearALLfilters
+    @FXML
+    private Button clearAllFilter;
+
+
+    // label for errors
+    @FXML
+    private Label a;
+
+
+
+
+    //@FXML
+   // public ArrayList<String> fillterSettings = ArrayList<String>(Arrays.asList(null,null,null,null,null));
+
+
+
+    @FXML
+    public void clearAllfilter(ActionEvent event){
+
+
+
+
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        a.setVisible(false);
+    }
 
 
 }
