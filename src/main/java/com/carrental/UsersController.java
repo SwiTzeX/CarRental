31daecfe324ffea6a66559a8eb347b9774ed1bf5
7amid <hh@hh>
2023-05-21 22:30:16 +0100

    Users-view.fxml update

--- src/main/java/com/carrental/UsersController.java
+++ src/main/java/com/carrental/UsersController.java
@@ -74,8 +74,8 @@ public class UsersController implements Initializable {
         else if (lists == invoicedate) filterSettings.set(1, lists.getValue());
         else if (lists == invoicestatue) filterSettings.set(2, lists.getValue());
 
-       roles = User.filters(filterSettings);
-       invoicestatue = User.filters(filterSettings);
+      // roles = User.filters(filterSettings);
+       //invoicestatue = User.filters(filterSettings);
 
     }
 
