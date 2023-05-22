ce9b421e137a4deee65baf71ca9db6a0733f3501
neima <neimahlou@gmail.com>
2023-05-22 15:43:11 +0100

    infopage-view

--- src/main/java/com/carrental/InfoPageController.java
+++ src/main/java/com/carrental/InfoPageController.java
@@ -1,13 +1,30 @@
 package com.carrental;
 
+import com.carrental.models.Reservation;
 import com.carrental.models.Vehicle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.fxml.Initializable;
+import javafx.stage.Stage;
 
-public class InfoPageController {
+import java.awt.event.MouseEvent;
+import java.io.IOException;
+import java.net.URL;
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.ZoneId;
+import java.util.Date;
+import java.util.ResourceBundle;
+
+public class InfoPageController implements Initializable{
 
     @FXML
     private Button btnBookNow;
@@ -60,7 +77,7 @@ public class InfoPageController {
     @FXML
     private Label username;
 
-    public void setData(Vehicle vehicle){
+    public void setData(Vehicle vehicle, Reservation reservation){
 
         Image image = new Image(getClass().getResourceAsStream(vehicle.getImage()));
         carpic.setImage(image);
@@ -71,25 +88,41 @@ public class InfoPageController {
         infoFuel.setText(vehicle.fuelType);
         infoGear.setText(vehicle.gearType);
         infoTrunk.setText(vehicle.getTrunkCapacity().toString());
-
-
-        /*
-        vehBrandModel.setText(vehicle.getBrandName() + " " + vehicle.getModelName());
-        vehType.setText(vehicle.getType());
-        vehPrice.setText(String.valueOf(vehicle.getPrice())+" DH");
-        vehPassengers.setText(String.valueOf(vehicle.getPassengers()));
-        vehMaxSpeed.setText(String.valueOf(vehicle.getMaxSpeed())+"km");
-        vehDeposit.setText(String.valueOf(vehicle.getDeposit())+"DH");
-        vehFuelType.setText(vehicle.getFuelType());
-        vehGearType.setText(vehicle.getGearType());
-        vehHorsePower.setText(String.valueOf(vehicle.getHorsePower()));
-        Image image = new Image(getClass().getResourceAsStream(vehicle.getImage()));
-        vehImage.setImage(image);
-        Image brandImage = new Image(getClass().getResourceAsStream(vehicle.getBrandImage()));
-        vehBrandImage.setImage(brandImage);
-        vehTrunkCapacityLabel.setText(String.valueOf(vehicle.getTrunkCapacity()));
-
-         */
+        infoPsngr.setText(vehicle.getPassengers().toString());
+        infoStartDate.setText(reservation.getStartDate().toString());
+        infoEndDate.setText(reservation.getEndDate().toString());
+        LocalDate first_date = convertToLocalDateViaInstant(reservation.getStartDate());
+        LocalDate second_date = convertToLocalDateViaInstant(reservation.getEndDate());
+        Period duration = Period.between(first_date,second_date);
+        infoDuration.setText(duration.toString());
+        infoPpd.setText(String.valueOf(vehicle.getPrice()));
+        infoTotP.setText(String.valueOf(reservation.totalPrice()));
     }
 
+    public LocalDate convertToLocalDateViaInstant(Date dateToConvert) {
+        return dateToConvert.toInstant()
+                .atZone(ZoneId.systemDefault())
+                .toLocalDate();
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        //setData();
+    }
+
+
+    /*
+    void onClickBookNow(){
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("Payment-view.fxml"));
+            Parent login = loader.load();
+            Stage stage =(Stage)((Node)event.getSource()).getScene().getWindow();
+            stage.setScene(new Scene(login));
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+     */
 }
