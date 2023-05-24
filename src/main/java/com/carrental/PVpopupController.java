16c64ab18f5e993e9b4136c744ef163dc0aeb8c2
MrQuack23 <90573610+MrQuack23@users.noreply.github.com>
2023-05-24 22:56:21 +0100

    POPUP_view button ok take back to home view

--- src/main/java/com/carrental/PVpopupController.java
+++ src/main/java/com/carrental/PVpopupController.java
@@ -1,4 +1,50 @@
 package com.carrental;
 
-public class PVpopupController {
+import com.carrental.models.Reservation;
+import com.carrental.models.Vehicle;
+import javafx.fxml.FXMLLoader;
+import java.awt.event.MouseEvent;
+import javafx.fxml.Initializable;
+import javafx.event.ActionEvent;
+import javafx.event.*;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+
+public class PVpopupController implements Initializable{
+
+    @FXML
+    private Button PopUp;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+
+    @FXML
+    public void onClickBackPV(ActionEvent event) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("home-view.fxml"));
+            Parent back = loader.load();
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            stage.setScene(new Scene(back));
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
