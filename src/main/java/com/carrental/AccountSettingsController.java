7af012a36e80558492dfca5adbf599f2d1782c7c
MrQuack23 <90573610+MrQuack23@users.noreply.github.com>
2023-06-12 11:57:23 +0100

    new interface to counsult the user details ( client or admin )

---
+++ src/main/java/com/carrental/AccountSettingsController.java
@@ -0,0 +1,71 @@
+package com.carrental;
+
+import com.carrental.models.Reservation;
+import com.carrental.models.User;
+import com.carrental.models.Vehicle;
+import javafx.animation.FadeTransition;
+import javafx.fxml.FXMLLoader;
+import java.awt.event.MouseEvent;
+import javafx.fxml.Initializable;
+import javafx.event.ActionEvent;
+import javafx.event.*;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.net.URL;
+import java.sql.*;
+import java.util.Date;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.effect.BoxBlur;
+import javafx.scene.effect.GaussianBlur;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.stage.StageStyle;
+import javafx.util.Duration;
+
+public class AccountSettingsController {
+
+    @FXML
+    private Label agevar;
+
+    @FXML
+    private ImageView avatarimg;
+
+    @FXML
+    private Button editb;
+
+    @FXML
+    private Label emailvar;
+
+    @FXML
+    private Label fullname1var;
+
+    @FXML
+    private Label fullnamevar;
+
+    @FXML
+    private Label nidvar;
+
+    @FXML
+    private Label phonevar;
+
+    @FXML
+    private Label statusvar;
+
+}
