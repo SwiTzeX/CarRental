c2f5c6d50b2b97b4e83b0b7a4c90b815f7aaba27
mythx <malakcristiano24@gmail.com>
2023-06-13 00:09:43 +0100

    login/register update

--- src/main/java/com/carrental/RegisterController.java
+++ src/main/java/com/carrental/RegisterController.java
@@ -51,10 +51,6 @@ public class RegisterController implements Initializable {
     @FXML
     private Label loginbtnid;
 
-/*    @FXML
-    private Label errorid;*/
-
-
     private boolean validatePassword(String password) {
         // Define regular expressions for different character types
         String lowercaseRegex = ".*[a-z].*";
@@ -62,26 +58,19 @@ public class RegisterController implements Initializable {
         String numberRegex = ".*\\d.*";
         String specialCharRegex = ".*[!@#$%^&*()_+\\-=[\\\\]{};':\"\\\\|,.<>/?].*";
 
-        // Check if the password meets the required criteria
         if (password.matches(lowercaseRegex) && password.matches(uppercaseRegex) &&
                 password.matches(numberRegex) && password.matches(specialCharRegex)) {
-            return true; // Password contains multiple character types
+            return true;
         } else {
-            return false; // Password does not meet the criteria
+            return false;
         }
     }
 
-
-
     private boolean validatePhoneNum(String phonen) {
-        // Define regular expressions for different character types
         String lowercaseRegex = ".*[a-z].*";
         String uppercaseRegex = ".*[A-Z].*";
         String specialCharRegex = ".*[!@#$%^&*()_+\\-=[\\\\]{};':\"\\\\|,.<>/?].*";
-        //String numberRegex = ".*\\d.*";
 
-
-        // Check if the password meets the required criteria
         if (phonen.matches(lowercaseRegex) || phonen.matches(uppercaseRegex) ||
                 phonen.matches(specialCharRegex)) {
             return true;
@@ -116,7 +105,6 @@ public class RegisterController implements Initializable {
         }
     }
 
-
     @FXML
     void register(ActionEvent event) {
         String name = fullnameid.getText();
@@ -126,60 +114,12 @@ public class RegisterController implements Initializable {
         String phone = phonenumid.getText();
         String password = passwordid.getText();
 
-
-
-        /*User u1 = User.getUserByEmail(email);
-        if(u1 != null){
-            errorid.setVisible(true);
-            errorid.setText("THIS EMAIL ALREADY EXISTS !");
-
-            return;
-        }
-        if (!password.equals(Vpasswordid.getText())) {
-            errorid.setVisible(true);
-            errorid.setText("Your password isn't the same !");
-
-            return;
-        }
-        if (password.length()<8) {
-            errorid.setVisible(true);
-            errorid.setText("Your password should contain at least 8 characters !");
-            return;
-        }
-
-        if (!validatePassword(password)) {
-            errorid.setVisible(true);
-            errorid.setText("Your password must contain:\n" +
-                    "\t•Upper case\n" +
-                    "\t•Lower case\n" +
-                    "\t•At least one number\n" +
-                    "\t•At least one special character");
-
-            return;
-        }
-
-        if (!ValidateEmail(email)) {
-            errorid.setVisible(true);
-            errorid.setText("Invalid email");
-
-            return;
-        }
-        //String numberRegex = ".*\\d.*";
-        if(validatePhoneNum(phone)){
-            errorid.setVisible(true);
-            errorid.setText("needs to contain only numbers");
-            return;
-        }
-
-*/
         if (!Nid.isError() && !fullnameid.isError() && !ageid.isError() && !phonenumid.isError() && !mailid.isError() && !passwordid.isError() && !Vpasswordid.isError()){
             User user = User.create(nationalId,email,phone,Integer.parseInt(age),name,password,0);
             try {
-                // Load the FXML file for the second view
                 FXMLLoader loader = new FXMLLoader(getClass().getResource("login-view.fxml"));
                 Parent login = loader.load();
 
-                // Create a new stage and set the second view as the root
                 Stage stage =(Stage)((Node)event.getSource()).getScene().getWindow();
                 stage.setScene(new Scene(login));
                 stage.show();
@@ -187,18 +127,15 @@ public class RegisterController implements Initializable {
                 e.printStackTrace();
             }
         }
-
     }
 
     @FXML
     void goToLogin(MouseEvent event) {
 
         try {
-            // Load the FXML file for the second view
             FXMLLoader loader = new FXMLLoader(getClass().getResource("login-view.fxml"));
             Parent login = loader.load();
 
-            // Create a new stage and set the second view as the root
             Stage stage =(Stage)((Node)event.getSource()).getScene().getWindow();
             stage.setScene(new Scene(login));
             stage.show();
@@ -210,11 +147,9 @@ public class RegisterController implements Initializable {
     @FXML
     void gotohome(ActionEvent event) {
         try {
-            // Load the FXML file for the second view
             FXMLLoader loader = new FXMLLoader(getClass().getResource("Main-view.fxml"));
             Parent home = loader.load();
 
-            // Create a new stage and set the second view as the root
             Stage stage =(Stage)((Node)event.getSource()).getScene().getWindow();
             stage.setScene(new Scene(home));
             stage.show();
@@ -226,7 +161,6 @@ public class RegisterController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        // Check if the password meets the required criteria
         Nid.myFocusedProperty().addListener((observable, oldValue, newValue) -> {
             if (!newValue) {
                 boolean test2 = ValidateNid(String.valueOf(Nid.getText()));
@@ -295,7 +229,6 @@ public class RegisterController implements Initializable {
         });
         Vpasswordid.myFocusedProperty().addListener((observable, oldValue, newValue) -> {
             if (!newValue) {
-
                     boolean test = Vpasswordid.getText().matches(passwordid.getText());
                     if (test)
                     Vpasswordid.hideError();
@@ -306,19 +239,14 @@ public class RegisterController implements Initializable {
         });
         phonenumid.myFocusedProperty().addListener((observable, oldValue, newValue) -> {
             if (!newValue) {
-               // System.out.println(Integer.parseInt(phonenumid.getText()));
                 try {
                     long phone = Long.parseLong(phonenumid.getText());
-                    /*if(validatePhoneNum(String.valueOf(phone))){
-                        phonenumid.showError("needs to contain only numbers");
-                    }*/
                     phonenumid.hideError();
                 } catch (Exception ignored) {
                     phonenumid.showError("needs to contain only numbers");
                 }
             }
         });
-
     }
 
     public void homeButton(ActionEvent event) {
